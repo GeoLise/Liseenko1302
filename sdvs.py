dfab309cb8ba39a7050350fc61ffc3fdd7d1b5e2
GeoLise <101557691+GeoLise@users.noreply.github.com>
2022-03-14 14:08:17 +0300

    Update sdvs.py

--- sdvs.py
+++ sdvs.py
@@ -1,9 +1,10 @@
 import telebot
 token = '5163401707:AAHqHabhzFXDz2gMFlrDZVabNEgHrO1UswY'
 bot = telebot.TeleBot(token)
+
 @bot.message_handler(content_types=["text"])
-def repeat_all_message(message):
+def repeat_all_messages(message): # Название функции не играет никакой роли
     bot.send_message(message.chat.id, message.text)
 
-if_name_=='_main_':
-    bot.infinity_polling()
+if __name__ == '__main__':
+     bot.infinity_polling()
