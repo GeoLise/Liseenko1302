f24963c97aff56d83e1ea463c6637a4f37af7ed5
GeoLise <101557691+GeoLise@users.noreply.github.com>
2022-03-16 09:09:36 +0300

    Update sdvs.py

--- sdvs.py
+++ sdvs.py
@@ -1,4 +1,5 @@
 import telebot
+from telebot import types
 token = '5163401707:AAHqHabhzFXDz2gMFlrDZVabNEgHrO1UswY'
 bot = telebot.TeleBot(token)
 
@@ -6,6 +7,12 @@ bot = telebot.TeleBot(token)
 def start_message(message):
     bot.send_message(message.chat.id,text="Привет, {0.first_name}!".format(message.from_user))
                      
-                     
+@bot.message_handler (commands=["button"])
+def button_message (message):
+    markup=types.ReplyKeyboardMarkup (resize_keyboard=True)
+    item1=types.KeyboardButton("Кнопка")
+    markup.add(item1)
+    bot.send_message(message.chat.id,"Ну да", reply_markup=markup)
+
 if __name__ == '__main__':
      bot.infinity_polling()
