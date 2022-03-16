353091469e4681005040715b3bba2250dedf5ed2
GeoLise <101557691+GeoLise@users.noreply.github.com>
2022-03-16 08:57:28 +0300

    Update sdvs.py

--- sdvs.py
+++ sdvs.py
@@ -2,10 +2,9 @@ import telebot
 token = '5163401707:AAHqHabhzFXDz2gMFlrDZVabNEgHrO1UswY'
 bot = telebot.TeleBot(token)
 
-@bot.message_handler(commands=["t"])
+@bot.message_handler(commands=["start"])
 def start_message(message):
-    bot.send_message(message.chat.id,text="Привет, {0.
-first_name}!".format(message.from_user))
+    bot.send_message(message.chat.id,text="Привет,")
                      
                      
 if __name__ == '__main__':
