42181c9791ab83d9cf50db92ad56f638da719fbf
GeoLise <101557691+GeoLise@users.noreply.github.com>
2022-03-16 08:54:02 +0300

    Update sdvs.py

--- sdvs.py
+++ sdvs.py
@@ -1,12 +1,15 @@
 import telebot
 token = '5163401707:AAHqHabhzFXDz2gMFlrDZVabNEgHrO1UswY'
 bot = telebot.TeleBot(token)
-@bot.message_handler(commands=['t'])
+
+@bot.message_handler(commands=["t"])
 def start_message(message):
     bot.send_message(message.chat.id,text="Привет, {0.
 first_name}!".format(message.from_user))
+                     
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message): # Название функции не играет никакой роли
     bot.send_message(message.chat.id, message.text)
+                     
 if __name__ == '__main__':
      bot.infinity_polling()
