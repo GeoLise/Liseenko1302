9f5eb8ac9e2e304556b1bf7c6a31030d970348b8
GeoLise <101557691+GeoLise@users.noreply.github.com>
2022-03-16 10:03:55 +0300

    Update sdvs.py

--- sdvs.py
+++ sdvs.py
@@ -6,14 +6,10 @@ bot = telebot.TeleBot(token)
 @bot.message_handler(commands=["start"])
 def start_message(message):
     bot.send_message(message.chat.id,text="Привет, {0.first_name}!".format(message.from_user))
-                     
-@bot.message_handler(commands=["button"])
-def button_message(message):
     markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
     item1=types.KeyboardButton("Кнопка")
     item2=types.KeyboardButton("Кнопка2")
-    markup.add(item1, item2)
-    bot.send_message(message.chat.id,'Ну да', reply_markup=markup)
+    markup.add(item1, item2)                 
 
 @bot.message_handler(content_types='text')
 def message_reply(message):
