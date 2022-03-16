9a2790ab9af02894019e60ef34ee7a272ebcf4e1
GeoLise <101557691+GeoLise@users.noreply.github.com>
2022-03-16 09:43:15 +0300

    Update sdvs.py

--- sdvs.py
+++ sdvs.py
@@ -11,11 +11,14 @@ def start_message(message):
 def button_message(message):
     markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
     item1=types.KeyboardButton("Кнопка")
-    markup.add(item1)
+    item2=types.KeyboardButton("Кнопка2")
+    markup.add(item1, item2)
     bot.send_message(message.chat.id,'Ну да', reply_markup=markup)
-    item2=types.KeyboardButton
-    markup.add(item1)
-    bot.send_message(message.chat.id,'Ага', reply_markup=markup)
 
+@bot.message_handler(content_types='text')
+def message_reply(message):
+    if message.text=="Кнопка":
+        bot.send_photo(message.chat.id, photo=open('img/ph.jpg', 'rb'))
+        
 if __name__ == '__main__':
      bot.infinity_polling()
